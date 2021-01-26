08d05c03bd3dd53b92cf58da7bbc0a163eff4f28
Aviv Levi <AvivLevi815@gmail.com>
2021-01-26 14:31:10 +0200

    create map

---
+++ src/Ignore/GAPI.js
@@ -0,0 +1,2 @@
+const  key='AIzaSyBfJj3AM9wSAy-s4XkW2SToZTNPyi1AMtc';
+export default key;
---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import key from '../Ignore/GAPI'
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  render() {
+      console.log(key)
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: -1.2884,
+            lng: 36.8233
+          }
+        }
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: key
+})(MapContainer);
