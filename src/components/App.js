30fffb6ba3567ee53f518fe80713aea1768170ac
Aviv Levi <AvivLevi815@gmail.com>
2021-01-27 10:25:31 +0200

    add blue dot marker for user location

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,63 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import key from '../Ignore/GAPI'
 
 const mapStyles = {
-  width: '100%',
-  height: '100%'
+    width: '100%',
+    height: '100%'
 };
 
 export class MapContainer extends Component {
-    state = { lat: null, lng:null, errorMessage: '' };
+    state = { lat: null, lng: null, errorMessage: '' };
 
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-          position => this.setState({ lat: position.coords.latitude ,lng : position.coords.longitude}),
-          err => this.setState({ errorMessage: err.message })
+            position => this.setState({ lat: position.coords.latitude, lng: position.coords.longitude }),
+            err => this.setState({ errorMessage: err.message })
         );
-      }
-      renderContent() {
+    }
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
-          return <div>Error: {this.state.errorMessage}</div>;
+            return <div>Error: {this.state.errorMessage}</div>;
         }
-    
+
         if (!this.state.errorMessage && this.state.lat) {
-          return (
-            <Map
-            google={this.props.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={
-              {
-                // lat: this.state.lat,
-                // lng: this.state.lng
-                lat: this.state.lat,
-                lng: this.state.lng
-              }
-            }
-          />  
-                );   
-               }
-    
+            return (
+                <div>
+                    <Map
+                        google={this.props.google}
+                        zoom={14}
+                        showsUserLocation={true}
+                        style={mapStyles}
+                        initialCenter={
+                            {
+                                lat: this.state.lat,
+                                lng: this.state.lng
+                            }
+                        }
+                    >
+                        <Marker
+                            icon={{url: "https://www.robotwoods.com/dev/misc/bluecircle.png"}}
+                            position={{
+                                lat: this.state.lat ,
+                                lng: this.state.lng }}
+                        />
+
+                    </Map>
+
+                </div>
+            );
+        }
+
         return <div> 'Please accept location request' </div>;
-      }
-  render() {
-      console.log(this.state);
-    
+    }
+    render() {
+        console.log(this.state);
+
         return <div>{this.renderContent()}</div>;
-        
-  }
+
+    }
 }
 
-export default GoogleApiWrapper({apiKey: key})(MapContainer);
+export default GoogleApiWrapper({ apiKey: key })(MapContainer);
