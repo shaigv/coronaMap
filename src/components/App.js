d9a1f3b98a4b7d4cad94a454254d7262db3f1bdd
shaig9@gmail.com <shaig9@gmail.com>
2021-01-28 11:58:39 +0200

    change map to hebrew and show marker details

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
-    state = { lat: null, lng: null, errorMessage: '' };
+    state = { lat: null, lng: null, errorMessage: '', showingInfoWindow: false, activeMarker: {}, selectedCovid: {} };
 
 
     componentDidMount() {
@@ -17,8 +17,26 @@ export class MapContainer extends Component {
             position => this.setState({ lat: position.coords.latitude, lng: position.coords.longitude }),
             err => this.setState({ errorMessage: err.message })
         );
-        
+
     }
+    onMarkerClick = (props, marker, e, covid) => {
+        console.log(covid)
+        this.setState({
+            selectedCovid: covid,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+
+    }
+
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
     renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
@@ -41,13 +59,27 @@ export class MapContainer extends Component {
                         }
                     >
                         <Marker
-                            icon={{url: "https://www.robotwoods.com/dev/misc/bluecircle.png"}}
+                            icon={{ url: "https://www.robotwoods.com/dev/misc/bluecircle.png" }}
                             position={{
-                                lat: this.state.lat ,
-                                lng: this.state.lng }}
+                                lat: this.state.lat,
+                                lng: this.state.lng
+                            }}
                         />
-                        
-                       {this.renderCovids()} 
+
+                        {this.renderCovids()}
+
+                        <InfoWindow 
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}
+                            onClose={this.onClose}
+                        >
+                            <div>
+                                    <div className='header'>{this.state.selectedCovid.name}</div>
+                                    <div className='content'>{this.state.selectedCovid.address}</div>
+                                    <i class="question icon" href="www.fsdf.com"></i>           
+                            </div>
+                            
+                        </InfoWindow>
 
                     </Map>
 
@@ -58,27 +90,33 @@ export class MapContainer extends Component {
         return <div> 'Please accept location request' </div>;
     }
 
-    renderCovids(){
+    renderCovids() {
         return covids_locations.map(covid => {
             return (
-            <Marker
-                icon={{url: "https://www.brainpop.com//image_library/287000/287812_256x256.png",
-                scaledSize:  new this.props.google.maps.Size(covid.datetime.length*10,covid.datetime.length*10)
-            }}
-                position={covid.location}
-                 />);
+                <Marker
+                    key={covid.name}
+                    // onClick={this.onMarkerClick}
+                    onClick={(props, marker, e) => this.onMarkerClick(props, marker, e, covid)}
+                    name={covid.name}
+                    icon={{
+                        url: "https://www.brainpop.com//image_library/287000/287812_256x256.png",
+                        scaledSize: new this.props.google.maps.Size(covid.datetime.length * 10, covid.datetime.length * 10)
+                    }}
+                    position={covid.location}
+                />);
         })
     }
+
     render() {
 
         return (
-        <div>
-            {this.renderContent()}
-            {/* {this.renderCovids()} */}
-        </div>
+            <div>
+                {this.renderContent()}
+                {/* {this.renderCovids()} */}
+            </div>
         );
 
     }
 }
 
-export default GoogleApiWrapper({ apiKey: key })(MapContainer);
+export default GoogleApiWrapper({ apiKey: key, language: 'iw' })(MapContainer);
